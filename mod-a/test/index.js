9cde5e306bd3c48f15cd4fe5eec60037af642e74
Buddhike de Silva <buddyspike@geeksdiary.com>
2018-08-26 13:20:11 +1000

    Multiplication tests

--- mod-a/test/index.js
+++ mod-a/test/index.js
@@ -1,8 +1,12 @@
 const assert = require('chai').assert;
-const {add} = require('../src/index');
+const {add, mul} = require('../src/index');
 
 describe('tests', () => {
   it('should add', () => {
     assert.equal(add(1, 1), 2);
+  });
+
+  it('should multiply', () => {
+    assert.equal(mul(2, 3), 6);
   });
 });
